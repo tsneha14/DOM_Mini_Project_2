a8c2563927a03ae63817ff9b8f4d1fe6d56edabf
tsneha14 <snehathondangi@outlook.com>
2023-04-05 16:32:45 -0500

    Project 2

---
+++ main.js
@@ -0,0 +1,10 @@
+//Converter
+document.getElementById("kgsInput").addEventListener("input", e => {
+    let kgs = e.target.value;
+    document.getElementById("lbsOutput").innerText = kgs * 2.205;
+})
+
+document.getElementById("feetInput").addEventListener("input", e => {
+    let feet = e.target.value;
+    document.getElementById("cmsOutput").innerText = feet * 30.48;
+})
